7b322f76ccbc87f817cddd5cad7a961296097c7f
Krystian <Krystiandzikiewicz1995@gmail.com>
2024-02-13 16:12:13 +0100

    Zmiana zapasowej do masterNowe

--- CarWorkshop.Domain/Encje/CarWorkshopContactDetails.cs
+++ CarWorkshop.Domain/Encje/CarWorkshopContactDetails.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace CarWorkshop.Domain.Encje
 {
     public class CarWorkshopContactDetails
-    {
+    { 
         public int Id { get; set; } 
         public string? PhoneNumber { get; set; }
         public string? Street { get; set; }
